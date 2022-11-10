04436d1101a6f49a798399620865e93856dc509c
suxxin <suxxin0@gmail.com>
2022-11-10 11:13:59 +0900

    Section 5: validation v1

--- Section_5_MVC_2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ Section_5_MVC_2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -39,6 +39,36 @@ public class ValidationItemControllerV1 {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+
+        //검증 오류 결과 보관
+        Map<String, String> errors = new HashMap<>();
+
+        //검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            errors.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다.");
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+        }
+
+        //복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 " + resultPrice);
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if (!errors.isEmpty()) {
+            model.addAttribute("errors", errors);
+            return "validation/v1/addForm";
+        }
+
+        //검증 성공 후 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
