aa0d62eef48bc0a9923dda39d9e4029f278169f4
Sujin <suxxin0@gmail.com>
2022-11-12 22:31:01 +0900

    Section 5: validation v4 ItemSaveForm, ItemUpdateForm

--- Section_5_MVC_2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ Section_5_MVC_2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,15 +12,9 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)
     private Long id;
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
